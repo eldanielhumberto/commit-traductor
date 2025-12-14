ea7483dfe7ab74a587db775249c224d58a63f995
eldanielhumberto <gamekik92@gmail.com>
2025-12-14 12:07:38 -0600

    refactor: migrate project from TypeScript to Go with Cobra CLI.

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/eldanielhumberto/ct/cmd"
+
+func main() {
+	cmd.Execute()
+}
--- src/utils/renameCommit.ts
+++
@@ -1,6 +0,0 @@
-import translate from 'translate';
-
-export const renameCommit = async (msg: string): Promise<string> => {
-	translate.engine = 'google';
-	return await translate(msg, { from: 'es', to: 'en' });
-};
